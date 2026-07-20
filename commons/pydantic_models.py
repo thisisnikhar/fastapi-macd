73a517306d256a83bcbcbb9747e3ddd72cbfc567
thisisnikhar <nikharsachdeva14@gmail.com>
2026-07-20 09:57:04 +0530

    Added feature to add CIOnboarding request to database. It currently supports only one server record in a request. Need to modify to enable multiple server records input in one request

--- commons/pydantic_models.py
+++ commons/pydantic_models.py
@@ -12,3 +12,15 @@ class User(BaseModel):
 class Authenticate(BaseModel):
     username: str = Field(min_length=5)
     password: str = Field(min_length=5)
+
+
+class CIOnboardingRequest(BaseModel):
+    ticket_type: str = Literal["ci"]
+    ip_address: str = Field(min_length=7,max_length=80)
+    hostname: str = Field(min_length=3,max_length=80)
+    serial_number: str = Field(min_length=3,max_length=80)
+    operating_system: str = Field(min_length=3, max_length=80)
+    os_version: str = Field(min_length=3,max_length=80)
+    cpu: int = Field(lt=1000,gt=0)
+    memory: int = Field(lt=100_000,gt=0)
+    hard_disk: int = Field(lt=10000,gt=0)
--- routers/ci_onboarding.py
+++ routers/ci_onboarding.py
@@ -1,7 +1,12 @@
+from datetime import datetime
+from ipaddress import ip_address
+
 from fastapi import APIRouter, Depends
-from commons.models import RequestData
+from commons.models import RequestData,CIOnboardingServerData
 from commons.db_dependency import db_dependency
 from commons.auth import get_current_user
+from commons.pydantic_models import CIOnboardingRequest
+from sqlalchemy import select,or_,func
 
 
 ci_onboarding_router = APIRouter()
@@ -13,5 +18,40 @@ async def get_all_ci_requests(db:db_dependency,current_user=Depends(get_current_
 
 
 @ci_onboarding_router.post("/request")
-async def create_request(db:db_dependency,current_user=Depends(get_current_user)):
-    return "This is not ready yet"
+async def create_request(request_data:CIOnboardingRequest,db:db_dependency,current_user=Depends(get_current_user)):
+    query = select(func.max(RequestData.ticket_id))
+    max_ticket_id = db.execute(query).scalar()
+    if max_ticket_id is None:
+        ticket_id = 1
+    else:
+        ticket_id = int(max_ticket_id) + 1
+    year = datetime.now().strftime("%y")  # 26
+    ticket_number = f"SR-{year}-{ticket_id:05d}"
+    # return request_data.ticket_type
+    # Updating RequestData table
+    request = RequestData(
+        ticket_id=ticket_id,
+        ticket_number=ticket_number,
+        ticket_type = request_data.ticket_type,
+        user_id = current_user.id
+    )
+    db.add(request)
+    db.commit()
+
+    # Updating CIOnboardingServerData Table
+    ci_onboarding_server_data = CIOnboardingServerData(
+        id=1,
+        record_id=1,
+        ip_address=request_data.ip_address,
+        hostname=request_data.hostname,
+        serial_number = request_data.serial_number,
+        operating_system = request_data.operating_system,
+        os_version = request_data.os_version,
+        cpu = request_data.cpu,
+        memory = request_data.memory,
+        hard_disk = request_data.hard_disk
+    )
+    db.add(ci_onboarding_server_data)
+    db.commit()
+
+    return {"ticket_id":ticket_id,"request_data":request_data}
