96aa6e732f64405a89602a7f8e894520307ab33c
thisisnikhar <nikharsachdeva14@gmail.com>
2026-07-23 10:55:20 +0530

    updated response models for tech onboarding

--- routers/tech_onboarding.py
+++ routers/tech_onboarding.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter,HTTPException
 from starlette import status
 from commons.db_dependency import db_dependency
 from commons.models import RequestData
-from commons.pydantic_models import TechOnboardingRequest
+from commons.pydantic_models import TechOnboardingRequest,TechResponseList
 from commons.user_dependency import current_user_dependency
 from commons.utilities import generate_new_ticket_id_and_ticket_number,add_tech_onboarding_server_data,generate_tech_response_data
 
@@ -10,7 +10,7 @@ from commons.utilities import generate_new_ticket_id_and_ticket_number,add_tech_
 tech_onboarding_router = APIRouter()
 
 
-@tech_onboarding_router.get("/",summary="Get all the Tech Onboarding Requests",status_code=status.HTTP_200_OK)
+@tech_onboarding_router.get("/",summary="Get all the Tech Onboarding Requests",status_code=status.HTTP_200_OK,response_model=TechResponseList)
 async def get_all_tech_requests_data(db: db_dependency,current_user=current_user_dependency):
     if current_user.role not in ["admin","ticket_admin"]:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
@@ -42,7 +42,7 @@ async def create_request(request_data:TechOnboardingRequest,db:db_dependency,cur
     return {"ticket_id":ticket_id,"request_data":request_data}
 
 
-@tech_onboarding_router.get("/my-requests",summary="Get the Tech Onboarding Requests for the current user",status_code=status.HTTP_200_OK)
+@tech_onboarding_router.get("/my-requests",summary="Get the Tech Onboarding Requests for the current user",status_code=status.HTTP_200_OK,response_model=TechResponseList)
 async def get_all_tech_requests_data_current_user(db:db_dependency,
                                                 current_user=current_user_dependency):
     requests = db.query(RequestData).filter(RequestData.ticket_type=="tech",
