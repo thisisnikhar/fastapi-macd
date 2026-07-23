6b38c3df3a8798e452c23e9b1eeec440dc22c9f6
thisisnikhar <nikharsachdeva14@gmail.com>
2026-07-23 08:51:51 +0530

    Added feature to fetch the tech/ci server detaiuls in Fetch a particular ticket

--- routers/macd.py
+++ routers/macd.py
@@ -49,5 +49,48 @@ async def update_ticket_status(request_data:StatusUpdate,
 async def get_ticket_details(ticket_number:str,
                                 db:db_dependency,
                                 current_user = current_user_dependency):
-    ticket_data = db.query(RequestData).filter()
-    return ticket_number
+    ticket_data = db.query(RequestData).filter(
+        RequestData.ticket_number == ticket_number
+    ).first()
+
+    ticket_response = {
+        "ticket_number": ticket_data.ticket_number,
+        "ticket_type": ticket_data.ticket_type,
+        "status": ticket_data.status,
+        "status_updated_by": ticket_data.status_updated_by,
+        "username": ticket_data.users.username,
+        "user_email": ticket_data.users.email,
+    }
+
+    if ticket_data.ticket_type == "ci":
+        server_data = []
+        for server in ticket_data.ci_onboarding:
+            server_data.append(
+                {
+                    "record_id": server.record_id,
+                    "ip_address": server.ip_address,
+                    "hostname": server.hostname,
+                    "serial_number": server.serial_number,
+                    "operating_system": server.operating_system,
+                    "os_version": server.os_version,
+                    "cpu": server.cpu,
+                    "memory": server.memory,
+                    "hard_disk": server.hard_disk
+                }
+            )
+        ticket_response["server_data"] = server_data
+    else:
+        tech_data = []
+        for tech in ticket_data.tech_onboarding:
+            tech_data.append(
+                {
+                    "record_id": tech.record_id,
+                    "ip_address": tech.ip_address,
+                    "tech_type": tech.tech_type,
+                    "tech_name": tech.tech_name,
+                    "tech_version": tech.tech_version,
+                }
+            )
+        ticket_response["tech_data"] = tech_data
+
+    return ticket_response
